23f23c18cf5ef71161fab98fef4e874371259364
MiraidaOrozbaeva <orozbaeva_miraida@mail.ru>
2026-01-19 09:27:21 +0600

    My name

--- src/revertAndReset/RevertDemo.java
+++ src/revertAndReset/RevertDemo.java
@@ -3,5 +3,6 @@ package revertAndReset;
 public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("Demo1");
+        System.out.println(My name is Miraida);
     }
 }
